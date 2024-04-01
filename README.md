91622ea71a1171a7c3f9956d20d5cd0282a1aee5
oilulio <oilulio@users.noreply.github.com>
2024-04-01 08:26:17 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# Example Boilerplate React App exploiting public API
+## With whimsical Hitch Hiker's Guide To the Galaxy quotes in response to crime statistics
+
+Offers choice of police force location.  Attempts to make a quantification of risk  by looking at number of stop and searches (but API currently always returns ERR502, even for their example).  Hence instead randomly decides if it is better or worse and randomly cycles through appropriate quotes.
+
+Once a force is selected, allows sub selection of a Neighbourhood and retrieves (but does not show) the boundry.
+
+Retains list of visited forces and allows reselection.
+
+`npm install` then `npm run dev` or `npm run build` will setup.  Then `serve` will serve on localhost.
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path:path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.?jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new CopyWebpackPlugin({ 
+      patterns: [ 
+       { from: './public/favicon.ico' },
+      ]
+   })
+]
+}
